789eb9dba705574ebda7eaf8c67bb7de3bd379e0
nodoraiz <nodoraiz@gmail.com>
2014-05-27 10:57:17 +0200

    Initial commit

---
+++ src/main/java/com/elevenpaths/App.java
@@ -0,0 +1,13 @@
+package com.elevenpaths;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
